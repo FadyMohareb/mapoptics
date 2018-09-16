d8df182c933db7a899baf580e32170343425a5c0
FadyMohareb <f.mohareb@cranfield.ac.uk>
2018-09-17 01:37:16 +0300

    Update GUI to ensure smooth visualisation

--- UserInterface/QueryView.java
+++ UserInterface/QueryView.java
@@ -542,6 +542,8 @@ public class QueryView extends JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setPreferredSize(new java.awt.Dimension(0, 0));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
