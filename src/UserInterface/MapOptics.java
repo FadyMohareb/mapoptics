a1cee2c43ec4115252b8c1ff2a16eacb4d87f863
James <james-fox@mail.com>
2021-03-08 15:56:08 +0000

    Remove duplicated comment

--- src/UserInterface/MapOptics.java
+++ src/UserInterface/MapOptics.java
@@ -2986,7 +2986,6 @@ public class MapOptics extends JFrame {
                     MapOptics.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         // Create and display the form
         java.awt.EventQueue.invokeLater(() -> new MapOptics().setVisible(true));
