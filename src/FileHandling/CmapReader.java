cab7c70f19f52662c05b9e8cc85ce8c851abd953
James <james-fox@mail.com>
2021-03-10 11:51:37 +0000

    Add methods for validation and read summary data
    More robust validation method to ensure correct file format is selected. getSummaryData method added to pull data required for the summary view table.

--- src/FileHandling/CmapReader.java
+++ src/FileHandling/CmapReader.java
@@ -1,16 +1,103 @@
 package FileHandling;
 
-import DataTypes.*;
+import DataTypes.ContigInfo;
+import DataTypes.LabelInfo;
+
+import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
-import javax.swing.JOptionPane;
+import java.util.List;
 
 /*
  * @author Josie
  */
 public class CmapReader {
+
+    public static boolean validateCmap (String filePath) {
+
+        if (filePath.endsWith(".cmap")) {
+            if (Files.exists(Paths.get(filePath))) {
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(filePath));
+                    String line;
+
+                    while ((line = br.readLine()) != null && line.startsWith("#")) {
+                        if (line.toLowerCase().contains("cmap file version")) {
+                            return true;
+                        }
+                    }
+
+                    //Show error message if wrong format
+                    JOptionPane.showMessageDialog(null,
+                            "Error loading CMAP file." +
+                                    "\n\nInvalid format!",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //Show error message if no file found
+                JOptionPane.showMessageDialog(null,
+                        "Error loading CMAP file." +
+                                "\n\nFile does not exist!",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            //Show error message if wrong file type
+            JOptionPane.showMessageDialog(null,
+                    "Error loading CMAP file." +
+                            "\n\nInvalid file type!",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+        return false;
+    }
+
+    public static List<List<Object>> getSummaryData(File cmapFile, List<Integer> refIds) {
+
+        List<List<Object>> summaryData = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(cmapFile));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#")) {
+                    continue;
+                }
+
+                String[] rowData = line.split("\t");
+                int id = Integer.parseInt(rowData[0]);
+
+                if (refIds.contains(id)) {
+                    refIds.remove(Integer.valueOf(id));
+                    double length = Double.parseDouble(rowData[1]);
+                    int labels = Integer.parseInt(rowData[2]);
+                    double density = (labels / length) * 100000;
+
+                    List<Object> data = new ArrayList<>(Arrays.asList(id, length, labels, density));
+
+                    summaryData.add(data);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return summaryData;
+    }
 
     public static LinkedHashMap cmapToHashMap(String filename) {
         String line;
@@ -31,7 +118,7 @@ public class CmapReader {
         LabelInfo labelInfo;
 
         // Check the file is a cmap file
-        if (filename.contains(".cmap")) {
+        if (filename.endsWith(".cmap")) {
             // Try to read file and assign variable to hashmap of contigs
             try {
                 BufferedReader reader = new BufferedReader(new FileReader(filename));
