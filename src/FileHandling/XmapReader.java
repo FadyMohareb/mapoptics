7387b83c8569e3fed9483ad272ad8585cad35d73
James <james-fox@mail.com>
2021-03-15 11:23:27 +0000

    Fix issue when contigs reversed
    Start and stop regions may be wrong way round if orientation is -. Fix reorientates start and stop to prevent issues counting overlaps. Comments added in getReferenceData in preparation for storing data for query view table.

--- src/FileHandling/XmapReader.java
+++ src/FileHandling/XmapReader.java
@@ -12,7 +12,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /*
