025c0638a85255a913a5226a45196b89764e55bc
Glynn Martin <glynn.martin@horizondiscovery.com>
2021-03-25 14:46:20 +0000

    Moving overlapping alignments down

--- src/Datasets/Default/RefViewData.java
+++ src/Datasets/Default/RefViewData.java
@@ -10,10 +10,7 @@ import FileHandling.XmapReader;
 import UserInterface.ModelsAndRenderers.MapOpticsModel;
 
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /*
     @author Josie
@@ -124,6 +121,7 @@ public class RefViewData {
                 highestOffSetX = maxX;
             }
         }
+
 
 
         // Slot in method with lowestOffSetX, highestOffsetY
