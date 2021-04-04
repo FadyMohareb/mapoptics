e793c5d4d42b18e45ec39436b5af8d65b44e80ae
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-04 19:57:07 +0100

    Add method to color label sites using CIGAR

--- src/Algorithms/Variants.java
+++ src/Algorithms/Variants.java
@@ -1,21 +1,26 @@
 package Algorithms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Variants {
 
     private final String hitEnum;
-    private final List<String> qrySiteCig;
+    private final List<String> parsedCigar;
+    private final Map<Integer, String> cigRefSites;
+    private final Map<Integer, String> cigQrySites;
+
+
 
     public Variants(String hitEnum) {
         this.hitEnum = hitEnum;
-        qrySiteCig = new ArrayList<>();
+        parsedCigar = new ArrayList<>();
+        cigRefSites = new HashMap<>();
+        cigQrySites = new HashMap<>();
 
     }
-    public List<String> parseHitEnum() {
+    public void parseHitEnum() {
         // create regex pattern (for 1 or more digits followed by a letter (non-digit)
         Pattern pattern = Pattern.compile("\\d+\\D");
         Matcher m = pattern.matcher(hitEnum);
@@ -27,17 +32,60 @@ public class Variants {
             String var = hitEnum.substring(m.start(), m.end());
             cigPairs.add(var);
         }
-        // ma
+        // Change extract number from each Cigar variant and use it as frequency in list
         for (String iter : cigPairs) {
             int freq = Integer.parseInt(iter.substring(0, iter.length() - 1));
             for (int i = 0; i < freq; i++) {
-                qrySiteCig.add(iter.substring(iter.length() - 1));
+                parsedCigar.add(iter.substring(iter.length() - 1));
             }
 
 
         }
-        System.out.println(qrySiteCig + " ");
-        return qrySiteCig;
+    }
+
+    public void colorCigSites(Map<Integer, Double> refSites, Set<Integer> qrySites, Double startPos,
+                              Double endPos) {
+        // Loop through cigar string, ref and qry sites
+        // convert sets to lists for easier access
+        List<Integer> refSitesSub = new ArrayList<>();
+        // extract relevant subset of ref sites for query region
+        refSites.forEach((key, value) -> {
+            if (value >= startPos && value <= endPos) {
+                refSitesSub.add(key);
+            }
+        });
+        Iterator<Integer> refIter = refSitesSub.iterator();
+        Iterator<String> cigIter = parsedCigar.iterator();
+        Iterator<Integer> qryIter = qrySites.iterator();
+        while (cigIter.hasNext()) {
+                switch (cigIter.next()) {
+                    case "M" -> {
+                        if (qryIter.hasNext() && refIter.hasNext()) {
+                            cigRefSites.put(refIter.next(), "M");
+                            cigQrySites.put(qryIter.next(), "M");
+                        }
+                    }
+                    case "D" -> {
+                        if (refIter.hasNext()) {
+                            cigRefSites.put(refIter.next(), "D");
+                        }
+                    }
+                    case "I" -> {
+                        if (qryIter.hasNext()) {
+                            cigQrySites.put(qryIter.next(), "I");
+                        }
+                }
+            }
+        }
+
+    }
+
+    public Map<Integer, String> getCigRefSites() {
+        return cigRefSites;
+    }
+
+    public Map<Integer, String> getCigQrySites() {
+        return cigQrySites;
     }
 
     // loop throught the alignments in pairs and compute the distances within each alignment pair
@@ -45,10 +93,5 @@ public class Variants {
     // draw SV region filled polygon
     // add SV metrics to other class
 
-    /*
-    Boolean isInversion(String orientation) {
-        // check for an inversion
-        return orientation.equals("-");
     }
-     */
-}
+
