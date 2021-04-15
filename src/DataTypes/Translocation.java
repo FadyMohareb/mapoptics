c773f673ac91a56d68e527fb3f019e63bea58a74
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-15 13:33:35 +0100

    Add translocation class for Translocation data

---
+++ src/DataTypes/Translocation.java
@@ -0,0 +1,17 @@
+package DataTypes;
+
+/*
+* @author Anisha
+*
+* */
+public class Translocation extends SV {
+
+    public Translocation() {
+        super();
+    }
+
+    @Override
+    void setSVRegion(SV sv) {
+
+    }
+}
