846e35a751f845e05b5966b77ecb2d19d9ce3e48
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-15 13:33:03 +0100

    Add inversion class for inversion data

---
+++ src/DataTypes/Inversion.java
@@ -0,0 +1,18 @@
+package DataTypes;
+
+/*
+ * @author Anisha
+ *
+ * */
+public class Inversion extends SV {
+
+    public Inversion() {
+        super();
+
+    }
+
+    @Override
+    void setSVRegion(SV sv) {
+
+    }
+}
