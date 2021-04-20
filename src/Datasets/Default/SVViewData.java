b54a025c8474bd7abc7d581780bdc86859ad4905
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-20 01:37:11 +0100

    Modify dataset for SVView for one reference querying

--- src/Datasets/Default/SVViewData.java
+++ src/Datasets/Default/SVViewData.java
@@ -1,9 +1,8 @@
 package Datasets.Default;
 
-import DataTypes.Duplication;
-import DataTypes.Indel;
-import DataTypes.Inversion;
-import DataTypes.Translocation;
+import Algorithms.DetectSV;
+import DataTypes.*;
+import FileHandling.CmapReader;
 import FileHandling.XmapReader;
 import UserInterface.ModelsAndRenderers.MapOpticsModel;
 
@@ -21,14 +20,7 @@ import java.util.Map;
 
 
 public class SVViewData {
-    private static List<String> refIDs;
-    private static String[] refInfo;
-    private String qryID;
-    private static Map<String,String[]> refData;
-    private static double RefLen;
-    private static double RefStart;
-    private static double QryLen;
-    private static double QryStart;
+
     public static List<Indel> indels = new ArrayList<>();
     public static List<Inversion> inversions = new ArrayList<>();
     public static List<Duplication> duplications = new ArrayList<>();
@@ -39,50 +31,15 @@ public class SVViewData {
         SVViewData.inversions.clear();
         SVViewData.duplications.clear();
         SVViewData.translocations.clear();
-        List<String> refIDs=new ArrayList<>();
-        Map<String,String[]> refData= new HashMap<>();
+
+
+    }
+
+    public static void setSVViewData(MapOpticsModel model, DetectSV detectSV) {
+        Reference ref = model.getSelectedRef();
+        DetectSV.setChosenRef(ref);
+        detectSV.detectIndels();
 
     }
 
-    public static void setSVViewData(MapOpticsModel model, String qryID) {
-        if(XmapReader.getQueryData(model, qryID) != null){
-            List<String> refIDs= new ArrayList<>();
-            refData= new HashMap<>();
-            for (String s : XmapReader.getQueryData(model, qryID).keySet()) {
-                refIDs.add(s);
-                refInfo=XmapReader.getQueryData(model, qryID).get(s);
-                refData.put(s,refInfo);
-            }
-        }else {refData=null;}
-
-    }
-    public static Map<String,String[]> getConnection(){
-        return refData;
-    }
-
-
-    public static void setSelectedRefLen(double reflen){
-        RefLen =reflen;
-    }
-    public static double getRefLen(){
-        return RefLen;
-    }
-    public static void setRefStart(double start){
-        RefStart=start;
-    }
-    public static double getRefStart(){
-        return RefStart;
-    }
-    public static void setSelectedQryLen(double qrylen){
-        QryLen =qrylen;
-    }
-    public static double getQryLen(){
-        return QryLen;
-    }
-    public static void setQryStart(double start){
-        QryStart=start;
-    }
-    public static double getQryStart(){
-        return QryStart;
-    }
 }
