86b97ee4368dd48538764058d5f9a924ce332f52
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-20 01:32:10 +0100

    Add constructor

--- src/DataTypes/Duplication.java
+++ src/DataTypes/Duplication.java
@@ -1,6 +1,16 @@
 package DataTypes;
 
+/*
+* @author Anisha
+*
+* Class for creating duplication objects and associated detection methods.
+*
+* */
 public class Duplication extends SV {
+
+    Duplication() {
+        super();
+    }
     @Override
     void setSVRegion(SV sv) {
 
