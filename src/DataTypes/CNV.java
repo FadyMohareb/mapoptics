074dd65210892992d6cf69ecc1d09ca77684908e
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-21 21:01:32 +0100

    Add type function

--- src/DataTypes/CNV.java
+++ src/DataTypes/CNV.java
@@ -6,6 +6,12 @@ public class CNV extends SV {
     public CNV() {
         super();
     }
+
+    @Override
+    public String setType() {
+        return "CNV";
+    }
+
     @Override
     void setSVRegion(SV sv) {
 
