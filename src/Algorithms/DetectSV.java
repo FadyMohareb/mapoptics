a09ea505d0b3bd37753e70d174381c00440a9038
a-thind <26285885+a-thind@users.noreply.github.com>
2021-04-24 18:38:37 +0100

    Fix duplication issue for indels

--- src/Algorithms/DetectSV.java
+++ src/Algorithms/DetectSV.java
@@ -32,7 +32,7 @@ public class DetectSV {
     private List<Indel> indels;
     private List<Inversion> inversions;
     private List<Duplication> duplications;
-    private List<Translocation> translocation;
+    private List<Translocation> translocations;
     private List<CNV> cnv;
     private List<SV> svList;
     private double pValueThresh;
@@ -42,7 +42,7 @@ public class DetectSV {
 
     public DetectSV(MapOpticsModel model) {
         this.model = model;
-        chosenRef = model.getSelectedRef();
+        chosenRef = null;
         this.minSupport = 1;
         this.minIndelSize = 500.0;
         this.maxIndelSize = 1000000.0;
@@ -76,8 +76,11 @@ public class DetectSV {
         this.FNR = FNR;
     }
 
-    public static void setChosenRef(Reference chosenRef) {
-        DetectSV.chosenRef = chosenRef;
+    public void setChosenRef(Reference chosenRef) {
+        this.chosenRef = chosenRef;
+    }
+    public static Reference getChosenRef() {
+        return chosenRef;
     }
 
     public void classifyLabels() {
@@ -86,44 +89,43 @@ public class DetectSV {
 
     public void detectIndels() {
         for (Query qry : chosenRef.getQueries()) {
-            System.out.println("refID: " + chosenRef.getRefID() + " qryID: " + qry.getID());
-            // for a given qry and ref get Alignment info
-            Map<Integer, Double> refSites = chosenRef.getSites();
-            Map<Integer, List<Integer>> qryAlignments = qry.getAlignmentSites();
-            Map<Integer, Double> qrySites = qry.getQryViewSites();
-            // Extract aligned ref sites with selected qry (matches on ref) with duplicate refsites removed
-            List<Integer> refAlignedSites = qryAlignments.values().stream().flatMapToInt(
-                    refSite -> refSite.stream().mapToInt(i -> i)).boxed().distinct().sorted()
-                    .collect(Collectors.toList());
-            System.out.println("qryAlignedSites" + qryAlignments);
-            int refStartSite = refAlignedSites.get(0);
-            // get the last site(s) to get the last site in case the last query site maps to multiple refSites
-            int  refEndSite = refAlignedSites.get(refAlignedSites.size() - 1);
+            if (!chosenRef.getDelQryIDs().contains(Integer.parseInt(qry.getID())) &&
+                    !chosenRef.getSavedDelQryIDs().contains(Integer.parseInt(qry.getID()))) {
+                // for a given qry and ref get Alignment info
+                Map<Integer, Double> refSites = chosenRef.getSites();
+                Map<Integer, List<Integer>> qryAlignments = qry.getAlignmentSites();
+                Map<Integer, Double> qrySites = qry.getQryViewSites();
+                // Extract aligned ref sites with selected qry (matches on ref) with duplicate refsites removed
+                List<Integer> refAlignedSites = qryAlignments.values().stream().flatMapToInt(
+                        refSite -> refSite.stream().mapToInt(i -> i)).boxed().distinct().sorted()
+                        .collect(Collectors.toList());
+                int refStartSite = refAlignedSites.get(0);
+                // get the last site(s) to get the last site in case the last query site maps to multiple refSites
+                int  refEndSite = refAlignedSites.get(refAlignedSites.size() - 1);
 
-            List<Indel> indels = Indel.getPutativeIndels(qryAlignments, chosenRef.getSites(),
-                    qry.getQryViewSites());
-            // cigar
-            Cigar cig = new Cigar(qry.getHitEnum());
-            cig.parseHitEnum();
+                List<Indel> indels = Indel.getPutativeIndels(qryAlignments, chosenRef.getSites(),
+                        qry.getQryViewSites());
+                // cigar
+                Cigar cig = new Cigar(qry.getHitEnum());
+                cig.parseHitEnum();
 
-            cig.mapCigSites(refSites, qrySites, qryAlignments);
-            Map<Integer, String> refCigar = cig.getCigRefSites();
-            Map<Integer, String> qryCigar = cig.getCigQrySites();
-            System.out.println("qryCigar" + qryCigar);
-            // filter the indels
-            List<Indel> filteredIndels = filterIndels(indels, this.minIndelSize, this.maxIndelSize, refCigar,
-                    this.flankSig);
-            // add qry and ref IDs to indels
-            for (Indel indel : filteredIndels) {
-                indel.setQryID(qry.getID());
-                indel.setRefID1(chosenRef.getRefID());
-                indel.setRefID2(chosenRef.getRefID());
-                indel.setType();
-                System.out.println(indel.getType());
+                cig.mapCigSites(refSites, qrySites, qryAlignments);
+                Map<Integer, String> refCigar = cig.getCigRefSites();
+                Map<Integer, String> qryCigar = cig.getCigQrySites();
+                // filter the indels
+                List<Indel> filteredIndels = filterIndels(indels, this.minIndelSize, this.maxIndelSize, refCigar,
+                        this.flankSig);
+                // add qry and ref IDs to indels
+                for (Indel indel : filteredIndels) {
+                    indel.setQryID(qry.getID());
+                    indel.setRefID1(chosenRef.getRefID());
+                    indel.setType();
+                }
+                System.out.println("detectsv loop: filteredIndels" + filteredIndels.size());
+                this.indels.addAll(filteredIndels);
+            } System.out.println("this.indels: line 126 " + this.indels);
             }
-            this.indels.addAll(filteredIndels);
         }
-    }
 
     // filter indels based on user-specified indel size range and flanking signals
     public static List<Indel> filterIndels(List<Indel> indelList, double minSize, double maxSize,
@@ -142,22 +144,56 @@ public class DetectSV {
                 }
             }
         }
+        System.out.println("filteredIndels: " + filteredIndels);
         return filteredIndels;
     }
     public List<Indel> getIndels() {
         return indels;
     }
 
+
+
+
     public void setIndels() {
-        detectIndels();
+        this.indels = new ArrayList<>();
     }
+
+    public SV getSV(String qryStartPos, String qryEndPos, String refStartPos, String refEndPos, String type) {
+        for (SV sv : this.svList) {
+            double qryStart = Double.parseDouble(qryStartPos);
+            double qryEnd = Double.parseDouble(qryEndPos);
+            double refStart = Double.parseDouble(refStartPos);
+            double refEnd = Double.parseDouble(refEndPos);
+            if (sv.qryStartPos == qryStart && sv.qryEndPos == qryEnd
+                    && sv.refStartPos == refStart && sv.refEndPos == refEnd && sv.type.equals(type)) {
+                return sv;
+            }
+        }
+        return null;
+    }
+
+    public List<Inversion> getInversions() {
+        return inversions;
+    }
+
+    public List<Duplication> getDuplications() {
+        return duplications;
+    }
+
+    public List<Translocation> getTranslocations() {
+        return translocations;
+    }
+
     public List<SV> getSVList() {
         return svList;
     }
 
     public void setSVList() {
         setIndels();
-        svList.addAll(indels);
-
+        detectIndels();
+        List<SV> svList = new ArrayList<>();
+        svList.addAll(this.indels);
+        this.svList = svList;
     }
+
 }
